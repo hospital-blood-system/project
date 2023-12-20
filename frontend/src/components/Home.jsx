9d2106dc42c3a478766b484ebcf104af660329d8
MustafaVural <1mustafavural1@gmail.com>
2023-12-20 21:13:16 +0300

    sayfa düzenlendi.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -69,66 +69,124 @@ function Home(){
       <div className="row justify-content-center">
         <div className="col-lg-6">
           <div className="section_title text-center mb-55">
-            <h3><span>Reason of Helping</span></h3>
+            <h3><span>İLANLAR</span></h3>
           </div>
         </div>
       </div>
       <div className="row justify-content-center">
-        <div className="col-lg-4 col-md-6">
-          <div className="single_reson">
-            <div className="thum">
-              <div className="thum_1">
-                <img src="img/help/1.png" alt />
-              </div>
-            </div>
-            <div className="help_content">
-              <h4>Collecting Fund</h4>
-              <p>Lorem ipsum, or lipsum as it is 
-                sometimes known, is dummy 
-                text used in laying out print.</p>
-              <a href="#" className="read_more">Read More</a>
-            </div>
+      <div className="card w-75 mb-3">
+  <div className="card-body">
+    <h5 className="card-title">🚨 ACİL KAN BAĞIŞI İHTİYACI 🚨</h5>
+    <p className="card-text">Sevgili Topluluk Üyeleri,
+
+Bir acil durumla karşı karşıyayız ve sizin yardımınıza ihtiyacımız var. Hasta bir bireyimiz hayati bir mücadele veriyor ve şu anda 🩸B Rh-  kana acil ihtiyaç duyuyoruz.
+
+Bağış Yeri ve Tarih:
+📅 Tarih: [21.12.2023]
+📍 Bağış Merkezi: [Fırat Üniversitesi Hastanesi]
+
+Lütfen sağlıklıysanız, bu çağrıya yanıt verin ve kan bağışında bulunun. Bir damla kan, bir hayat kurtarabilir. Ayrıca, bu duyuruyu çevrenizle paylaşarak daha fazla kişinin haberdar olmasına yardımcı olabilirsiniz.
+
+Unutmayın, birlikte daha güçlüyüz. Yardımlarınız için şimdiden teşekkür ederiz! 
+
+İletişim İçin:
+📞 [05416309501]
+✉️ [hasanözdinç@gmail.com] . . .</p>
+    <a href="#" class="btn btn-primary">Detay </a>
+  </div>
+</div>
+
+<div className="card w-75 mb-3">
+  <div className="card-body">
+    <h5 className="card-title">🚨 ACİL KAN BAĞIŞI İHTİYACI 🚨</h5>
+    <p className="card-text">Sevgili Topluluk Üyeleri,
+
+Bir acil durumla karşı karşıyayız ve sizin yardımınıza ihtiyacımız var. Hasta bir bireyimiz hayati bir mücadele veriyor ve şu anda 🩸0 Rh+  kana acil ihtiyaç duyuyoruz.
+
+Bağış Yeri ve Tarih:
+📅 Tarih: [17.12.2023]
+📍 Bağış Merkezi: [Fetih Sekin Şehir Hastanesi]
+
+Lütfen sağlıklıysanız, bu çağrıya yanıt verin ve kan bağışında bulunun. Bir damla kan, bir hayat kurtarabilir. Ayrıca, bu duyuruyu çevrenizle paylaşarak daha fazla kişinin haberdar olmasına yardımcı olabilirsiniz.
+
+Unutmayın, birlikte daha güçlüyüz. Yardımlarınız için şimdiden teşekkür ederiz!
+
+İletişim İçin:
+📞 [05436845485]
+✉️ [meryemnurozdemir@gmail.com] . . .</p>
+    <a href="#" class="btn btn-primary">Detay</a>
+ 
+
+
           </div>
         </div>
-        <div className="col-lg-4 col-md-6">
-          <div className="single_reson">
-            <div className="thum">
-              <div className="thum_1">
-                <img src="img/help/2.png" alt />
-              </div>
-            </div>
-            <div className="help_content">
-              <h4>Blood Camp</h4>
-              <p>Lorem ipsum, or lipsum as it is 
-                sometimes known, is dummy 
-                text used in laying out print.</p>
-              <a href="#" className="read_more">Read More</a>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6">
-          <div className="single_reson">
-            <div className="thum">
-              <div className="thum_1">
-                <img src="img/help/3.png" alt />
-              </div>
-            </div>
-            <div className="help_content">
-              <h4>Friendly Volunteer</h4>
-              <p>Lorem ipsum, or lipsum as it is 
-                sometimes known, is dummy 
-                text used in laying out print.</p>
-              <a href="#" className="read_more">Read More</a>
-            </div>
-          </div>
-        </div>
+        <div className="card w-75 mb-3">
+  <div className="card-body">
+    <h5 className="card-title">🌟 BAĞIŞ KAMPANYASI: BİRLİKTE HAYATLARI DEĞİŞTİRELİM! 🌟</h5>
+    <p className="card-text">
+Sevgili Dostlar,
+
+Sizleri yeni bir bağış kampanyamıza katılmaya davet ediyoruz! Bu kampanya ile birlikte, topluluğumuzun sağlığına ve refahına destek olabilir, birlikte güçlü bir etki yaratabiliriz.
+
+🏆 Hedefimiz: Bu kampanyada amacımız 1000 bağışçıdan destek almak
+
+📅 Kampanya Süresi: [1.12.2023] - [1.01.2024]
+
+Kampanya Avantajları:
+✨ İlk 100 Bağışçıya Özel Sertifika
+✨ Her Bağış, Topluluğumuz İçin Bir Yatırım
+
+Bağışı Paylaşın:
+Bağışınızın sosyal medyada #HayatıDeğiştirBağışıyla etiketiyle paylaşarak kampanyamıza daha fazla katkı sağlayabilirsiniz.
+
+🙏 Topluluğumuzun gücü, sizin bağışlarınızla büyüyor. Katkılarınız için şimdiden teşekkür ederiz!
+
+
+İletişim İçin:
+📞 [02124502052] . . .
+</p>
+    <a href="#" class="btn btn-primary">Detay</a>
+  </div>
+</div>
+
+<div className="card w-75 mb-3">
+  <div className="card-body">
+    <h5 className="card-title">🤝 GÖNÜLLÜ BAĞIŞ ÇAĞRISI: BİR GÜLÜMSEMEYLE DEĞİŞİM YARATIN! 🤝</h5>
+    <p className="card-text">
+Sevgili Dostlar,
+
+Topluluğumuzu daha güzel bir yer haline getirmek için bir araya gelme vakti geldi! Gelin, birlikte gönüllü olarak değişim yaratalım ve birbirimize destek olalım.
+
+👐 Gönüllü Hedefimiz: Bu kampanya kapsamında 500 gönüllüyü bir araya getirerek çeşitli projelerde birlikte çalışmak ve pozitif bir etki yaratmak.
+
+📅 Gönüllülük Tarihleri: [1.01.2024] - [30.06.2024]
+
+Neden Gönüllü Olmalısınız?
+[Burada, gönüllülük faaliyetinin topluluğa, çevreye veya belirli bir amaca nasıl katkı sağlayacağını açıklayın.]
+
+Gönüllülük Avantajları:
+✨ Yeni Arkadaşlıklar Kurma Fırsatı
+✨ Topluluğumuza Kalıcı Bir Etki Bırakma Şansı
+
+Gönüllülüğü Paylaşın:
+Gönüllülük deneyimlerinizi sosyal medyada #GönüllüDeğişim etiketiyle paylaşarak daha fazla insanı gönüllü olmaya teşvik edebilirsiniz.
+
+🙏 Topluluğumuza değerli katkılarınız için şimdiden teşekkür ederiz!
+İletişim İçin:
+📞 [02124502052] . . .
+</p>
+    <a href="#" class="btn btn-primary">Detay</a>
+  </div>
+</div>
+          
+       
       </div>
     </div>
   </div>
   {/* reson_area_end  */}
   {/* footer_start  */}
   <footer className="footer">
-    <div className="footer_top">
+    <div className="">
       <div className="container">
         <div className="row">
           <div className="col-xl-4 col-md-6 col-lg-4 ">
@@ -138,99 +196,67 @@ function Home(){
                   <img src="img/footer_logo.png" alt />
                 </a>
               </div>
-              <p className="address_text">Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit, sed do <br /> eiusmod tempor incididunt ut labore.
+              <p className="address_text"> <br />  <br /> 
               </p>
-              <div className="socail_links">
-                <ul>
-                  <li>
-                    <a href="#">
-                      <i className="ti-facebook" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i className="ti-twitter-alt" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i className="fa fa-dribbble" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i className="fa fa-instagram" />
-                    </a>
-                  </li>
-                </ul>
-              </div>
+              
             </div>
           </div>
-          <div className="col-xl-2 col-md-6 col-lg-2">
+
+          <div className="col-xl-5 col-md-15 col-lg-10">
             <div className="footer_widget">
               <h3 className="footer_title">
-                Services
-              </h3>
-              <ul className="links">
-                <li><a href="#">Donate</a></li>
-                <li><a href="#">Sponsor</a></li>
-                <li><a href="#">Fundraise</a></li>
-                <li><a href="#">Volunteer</a></li>
-                <li><a href="#">Partner</a></li>
-                <li><a href="#">Jobs</a></li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-xl-3 col-md-6 col-lg-3">
-            <div className="footer_widget">
-              <h3 className="footer_title">
-                Contacts
+                İletişim Bilgileri
               </h3>
               <div className="contacts">
-                <p>+2(305) 587-3407 <br />
-                  info@loveuscharity.com <br />
-                  Flat 20, Reynolds Neck, North
-                  Helenaville, FV77 8WS
+                <p>Telefon ☎️:
+                  0(424)218 1346 <br />
+                Email ✉️: 
+                info@kanverhayatkurtar.com <br />
+                  Adres: 
+                  Nailbey Mh. 12002. Sk. 
+                  No:1/14 Elazığ İş Merkezi Kat:3
+
+                  
                 </p>
               </div>
             </div>
           </div>
-          <div className="col-xl-3 col-md-6 col-lg-3">
-            <div className="footer_widget">
-              <h3 className="footer_title">
-                Top News
-              </h3>
-              <ul className="news_links">
-                <li>
-                  <div className="thumb">
-                    <a href="#">
-                      <img src="img/news/news_1.png" alt />
-                    </a>
-                  </div>
-                  <div className="info">
-                    <a href="#">
-                      <h4>School for African 
-                        Childrens</h4>
-                    </a>
-                    <span>Jun 12, 2019</span>
-                  </div>
-                </li>
-                <li>
-                  <div className="thumb">
-                    <a href="#">
-                      <img src="img/news/news_2.png" alt />
-                    </a>
-                  </div>
-                  <div className="info">
-                    <a href="#">
-                      <h4>School for African 
-                        Childrens</h4>
-                    </a>
-                    <span>Jun 12, 2019</span>
-                  </div>
-                </li>
-              </ul>
-            </div>
+          <div className="col-xl-10 col-md-5 col-lg-3">
+          <form>
+      <div className="row">
+        <div className="col">
+          <input type="text" className="form-control" placeholder="İsim" />
+        </div>
+        <div className="col">
+          <input type="text" className="form-control" placeholder="Soyisim" />
+        </div>
+      </div>
+    </form>
+          <form>
+  <div className="form-group">
+    
+  </div>
+  <div className="form-group">
+   
+  </div>
+  <form>
+  <form>
+  <div className="form-group">
+    <label htmlFor="formGroupExampleInput">Telefon :</label>
+    <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Telefon Girin..." />
+  </div>
+  <div className="form-group">
+    <label htmlFor="formGroupExampleInput2">E-Mail :</label>
+    <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="E-mail Girin..." />
+  </div>
+</form>
+</form>
+  <div className="form-group">
+    <label for="exampleFormControlTextarea1">Mesaj :</label>
+    <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+  </div>
+</form>
+<button type="button" class="btn btn-danger">Gönder</button>
           </div>
         </div>
       </div>
@@ -242,7 +268,7 @@ function Home(){
           <div className="col-xl-12">
             <p className="copy_right text-center">
             </p><p>{/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
-              Copyright © All rights reserved | This template is made with <i className="ti-heart" aria-hidden="true" /> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+            Telif Hakkı © 2023 ISS Ekibi Tarafından Tasarlanmıştır.Tüm Hakları Saklıdır.<i className="ti-heart" aria-hidden="true" /> by <a href=" INNOVATIVE SOLUTİONS AND SOFTWARE ŞTİ. AŞ." target="_blank"> INNOVATIVE SOLUTİONS AND SOFTWARE ŞTİ. AŞ.</a>
               {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}</p>
             <p />
           </div>
